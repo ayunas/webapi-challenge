c853827a3fe6c4f77531b7f4c8789a7f27146cdb
ayunas <ayunas@gmail.com>
2019-05-10 23:16:40 -0500

    app is funcitonal with exception of a presentational bug that displays empty <div>  below every nonselected action list

--- action-project/src/Projects.js
+++ action-project/src/Projects.js
@@ -8,16 +8,15 @@ export default class Projects extends Component {
         this.state = {
             actionFlag : false,
             actions : [],
-            active : false
+            hide : false
         }
     }
-
 
   action = (id) => {
       console.log('action triggered with id: ', id)
       this.setState({
           actionFlag : !this.state.actionFlag,
-          active : !this.state.active
+          hide : !this.state.hide
       })
       axios.get(`http://localhost:5353/projects/${id}/actions`)
       .then( res => {
@@ -28,17 +27,39 @@ export default class Projects extends Component {
         })
       .catch( err => console.log(err))
   }
-
+  
   render() {
     return (
       <div className='projects'>
         {this.props.projects.map( (project,i) => 
         <div key={i}>
-            <h3><small> {project.id } </small> {project.name}</h3>
+            <h3> <small> { project.id } </small>{project.name}</h3>
             <button onClick={ () => this.action(project.id)}>view project actions</button>
-            {this.state.actionFlag && this.state.actions.map( (action,j) => <div className={`${this.state.active} action`} key={`action-${j}`}>{ action.project_id === project.id && action.description}</div>) }
-        </div>)}
+            {this.state.actionFlag && this.state.actions.map( (action,j) => 
+            <div className='actions' key={`action-${j}`} > {action.project_id === project.id && action.description} </div> 
+              )
+            }      
+        </div>
+            )}
       </div>
     )
   }
 }
+
+
+
+//className={`${this.state.active} action`} in state:  active : false.  not needed for the empty divs anymore
+
+// ({action.project_id === project.id) ? 
+//   (<div className='actions' 
+//        key={`action-${j}`}>
+//       { action.project_id === project.id && action.description}
+//   </div>) : (
+
+  // return (
+  //   <div className='actions' 
+  //        key={`action-${j}`}>
+  //        { action.project_id === project.id && action.description}
+  //   </div>
+  // )
+  // <div className='actions' key={`action-${j}`}>{action.description}</div>
